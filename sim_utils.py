a76928a67042b42612c7f3ef928da2fce86bac09
Matthew Farrell <msfarrell@seas.harvard.edu>
2024-03-12 18:14:13 +0900

    sync

--- sim_utils.py
+++ sim_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import mujoco as mj
 
 class FilteredNoise:
     def __init__(self, ind_dim, kernel, seed=23):
@@ -12,3 +13,35 @@ class FilteredNoise:
         self.perturb[:] = np.roll(self.perturb, -1, axis=1)
         self.perturb[:, -1] = self.rng.standard_normal(self.ind_dim)
         return perturb_smoothed
+
+def traj_deriv(model, data, qs, vs, us, lams_fin, losses):
+    # WARNING: changes data!
+    Tk = qs.shape[0]
+    As = np.zeros((Tk, 2*model.nv, 2*model.nv))
+    Bs = np.zeros((Tk, 2*model.nv, model.nu))
+    Cs = np.zeros((Tk, 3, model.nv))
+    lams = np.zeros((Tk, 2*model.nv))
+
+    for tk in range(Tk):
+        data.qpos[:] = qs[tk]
+        data.qvel[:] = vs[tk]
+        data.ctrl[:] = us[tk]
+        epsilon = 1e-6
+        mj.mjd_transitionFD(model, data, epsilon, True, As[tk], Bs[tk], None,
+                            None)
+        # mj.mj_jacSite(model, data, Cs[tk], None, site=model.site('').id)
+
+    # dldq = Cs[Tk-1].T @ dlds
+    # lams[Tk-1,:nv] = targ_factor * dldq
+    # lams[Tk-1,2] += dldtheta
+    lams[Tk-1,:model.nq] = lams_fin
+    grads = np.zeros((Tk, model.nu))
+    tau_loss_factor = 1e-9
+
+    for tk in range(2, Tk):
+        lams[Tk-tk] = As[Tk-tk].T @ lams[Tk-tk+1]
+        grads[Tk-tk] = (tau_loss_factor/tk**.5)*losses[Tk-tk] \
+                + Bs[Tk-tk].T @ lams[Tk-tk+1]
+    return grads
+
+
--- test_humanoid2d.py
+++ test_humanoid2d.py
@@ -1,7 +1,8 @@
 import humanoid2d as h2d
 import baseball_lqr as lqr
 import numpy as np
-import sim_util as util
+import sim_utils as util
+import mujoco as mj
 
 seed = 2
 
@@ -19,7 +20,6 @@ env.reset(seed=seed)
 model = env.model
 data = env.data
 
-
 # Get initial stabilizing controls
 ## Burn in:
 for k in range(10):
@@ -30,14 +30,44 @@ ctrl0 = lqr.get_ctrl0(model, data)
 data.ctrl = ctrl0
 K = lqr.get_feedback_ctrl_matrix(model, data)
 
-CTRL_STD = 0.05       # actuator units
+# CTRL_STD = 0.05       # actuator units
+CTRL_STD = 0       # actuator units
 CTRL_RATE = 0.8       # seconds
 width = int(CTRL_RATE/model.opt.timestep)
 kernel = np.exp(-0.5*np.linspace(-3, 3, width)**2)
 kernel /= np.linalg.norm(kernel)
 noise = util.FilteredNoise(model.nu, kernel, 3*seed+7)
 
-for k in range(2000):
+Tk = 20
+qs = np.zeros((Tk, model.nq))
+qs[0] = qpos0
+vs = np.zeros((Tk, model.nq))
+vs[0] = data.qvel.copy()
+us = np.zeros((Tk, model.nu))
+losses = np.zeros((Tk,))
+
+for k in range(Tk-1):
     ctrl = lqr.get_lqr_ctrl_from_K(model, data, K, qpos0, ctrl0)
-    env.step(ctrl + CTRL_STD*noise.sample())
-# breakpoint()
+    out = env.step(ctrl + CTRL_STD*noise.sample())
+    observation, reward, terminated, __, info = out
+    qs[k+1] = observation[:model.nq]
+    vs[k+1] = observation[model.nq:]
+
+sites1 = data.site('hand_right').xpos
+sites2 = data.site('target').xpos
+dlds = sites1 - sites2
+# curr_loss = .5*np.linalg.norm(dlds)**2
+# Cs = np.zeros((Tk, 3, model.nv))
+C = np.zeros((3, model.nv))
+mj.mj_jacSite(model, data, C, None, site=data.site('hand_right').id)
+
+dldq = C.T @ dlds
+# lams_fin = np.zeros((Tk, model.nu))
+lams_fin = dldq
+
+# lams[Tk-1,:nv] = dldq
+# lams[Tk-1,2] += dldtheta
+
+grads = util.traj_deriv(model, data, qs, vs, us, lams_fin, losses)
+
+breakpoint()
