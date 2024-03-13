855f1703069cf79066d8f8025656a7ac5bf0ce9a
Matthew Farrell <msfarrell@seas.harvard.edu>
2024-03-13 20:05:10 +0900

    sync

--- sim_utils.py
+++ sim_utils.py
@@ -14,27 +14,38 @@ class FilteredNoise:
         self.perturb[:, -1] = self.rng.standard_normal(self.ind_dim)
         return perturb_smoothed
 
-def traj_deriv(model, data, qs, vs, us, lams_fin, losses):
+def traj_deriv(model, data, qs, vs, us, lams_fin, losses,
+               fixed_joint_inds=[]):
     # WARNING: changes data!
+    nqf = model.nq - len(fixed_joint_inds)
+    extra_inds = [i+model.nq for i in fixed_joint_inds]
+    fixed_joint_inds_2 = fixed_joint_inds + extra_inds
     Tk = qs.shape[0]
-    As = np.zeros((Tk, 2*model.nv, 2*model.nv))
-    Bs = np.zeros((Tk, 2*model.nv, model.nu))
-    Cs = np.zeros((Tk, 3, model.nv))
-    lams = np.zeros((Tk, 2*model.nv))
+    As = np.zeros((Tk, 2*nqf, 2*nqf))
+    Bs = np.zeros((Tk, 2*nqf, model.nu))
+    # Cs = np.zeros((Tk, 3, nvf))
+    
 
     for tk in range(Tk):
         data.qpos[:] = qs[tk]
         data.qvel[:] = vs[tk]
         data.ctrl[:] = us[tk]
         epsilon = 1e-6
-        mj.mjd_transitionFD(model, data, epsilon, True, As[tk], Bs[tk], None,
+        A = np.zeros((2*model.nq, 2*model.nq))
+        B = np.zeros((2*model.nq, model.nu))
+        mj.mjd_transitionFD(model, data, epsilon, True, A, B, None,
                             None)
+        A = np.delete(A, fixed_joint_inds_2, axis=0)
+        As[tk] = np.delete(A, fixed_joint_inds_2, axis=1)
+        B = np.delete(B, fixed_joint_inds_2, axis=0)
+        Bs[tk] = np.delete(B, fixed_joint_inds, axis=1)
         # mj.mj_jacSite(model, data, Cs[tk], None, site=model.site('').id)
 
+    lams = np.zeros((Tk, 2*nqf))
     # dldq = Cs[Tk-1].T @ dlds
     # lams[Tk-1,:nv] = targ_factor * dldq
     # lams[Tk-1,2] += dldtheta
-    lams[Tk-1,:model.nq] = lams_fin
+    lams[Tk-1,:nqf] = lams_fin
     grads = np.zeros((Tk, model.nu))
     tau_loss_factor = 1e-9
 
@@ -43,5 +54,4 @@ def traj_deriv(model, data, qs, vs, us, lams_fin, losses):
         grads[Tk-tk] = (tau_loss_factor/tk**.5)*losses[Tk-tk] \
                 + Bs[Tk-tk].T @ lams[Tk-tk+1]
     return grads
-
 
--- test_humanoid2d.py
+++ test_humanoid2d.py
@@ -7,7 +7,7 @@ import mujoco as mj
 seed = 2
 
 # # Create a Humanoid2dEnv object
-env = h2d.Humanoid2dEnv(render_mode='human')
+env = h2d.Humanoid2dEnv(render_mode='human', frame_skip=1)
 env.reset(seed=seed)
 
 # zac = 9*[0]
@@ -21,9 +21,13 @@ model = env.model
 data = env.data
 
 # Get initial stabilizing controls
-## Burn in:
-for k in range(10):
-    env.step(np.zeros(model.nu))
+## Reset and burn in:
+def reset(model, data, nsteps):
+    mj.mj_resetData(model, data)
+    for k in range(nsteps):
+        mj.mj_step(model, data)
+
+reset(model, data, 10)
 
 qpos0 = data.qpos.copy()
 ctrl0 = lqr.get_ctrl0(model, data)
@@ -38,20 +42,23 @@ kernel = np.exp(-0.5*np.linspace(-3, 3, width)**2)
 kernel /= np.linalg.norm(kernel)
 noise = util.FilteredNoise(model.nu, kernel, 3*seed+7)
 
-Tk = 20
+Tk = 200
 qs = np.zeros((Tk, model.nq))
 qs[0] = qpos0
 vs = np.zeros((Tk, model.nq))
 vs[0] = data.qvel.copy()
 us = np.zeros((Tk, model.nu))
 losses = np.zeros((Tk,))
+ctrls = np.zeros((Tk-1, model.nu))
 
 for k in range(Tk-1):
     ctrl = lqr.get_lqr_ctrl_from_K(model, data, K, qpos0, ctrl0)
+    ctrls[k] = ctrl
     out = env.step(ctrl + CTRL_STD*noise.sample())
     observation, reward, terminated, __, info = out
     qs[k+1] = observation[:model.nq]
     vs[k+1] = observation[model.nq:]
+
 
 sites1 = data.site('hand_right').xpos
 sites2 = data.site('target').xpos
@@ -68,6 +75,29 @@ lams_fin = dldq
 # lams[Tk-1,:nv] = dldq
 # lams[Tk-1,2] += dldtheta
 
-grads = util.traj_deriv(model, data, qs, vs, us, lams_fin, losses)
+# ctrls = ctrls - 0*grads[:Tk-1]
 
-breakpoint()
+while True:
+    sites1 = data.site('hand_right').xpos
+    sites2 = data.site('target').xpos
+    dlds = sites1 - sites2
+    C = np.zeros((3, model.nv))
+    mj.mj_jacSite(model, data, C, None, site=data.site('hand_right').id)
+
+    dldq = C.T @ dlds
+    lams_fin = dldq
+
+    reset(model, data, 10)
+    grads = util.traj_deriv(model, data, qs, vs, us, lams_fin, losses)
+    ctrls = ctrls - .01*grads[:Tk-1]
+
+    env.reset(seed=seed)
+    reset(model, data, 10)
+    for k in range(Tk-1):
+        ctrl = ctrls[k]
+        out = env.step(ctrl + CTRL_STD*noise.sample())
+        observation, reward, terminated, __, info = out
+        qs[k+1] = observation[:model.nq]
+        vs[k+1] = observation[model.nq:]
+
+
