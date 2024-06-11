a44f457cd5b64988e2e4dc9767ded4abfff22460
Matthew Farrell <matthew.farrell.235@gmail.com>
2024-06-11 21:36:47 +0800

    sync

--- fix_tennis_grab.py
+++ fix_tennis_grab.py
@@ -39,26 +39,31 @@ data = env.data
 
 outdir = Path('output')
 outdir.mkdir(parents=True, exist_ok=True)
-savedir = Path('data')
+# savedir = Path('data/phase_1')
+savedir = Path('data/phase_2')
+save_only_state = False
+# save_only_state = True
 savedir.mkdir(parents=True, exist_ok=True)
 
 ### Set things up
 num = 3
-seed = 2
-out_f = outdir/f'tennis_ctrl_grab_{num}.pkl'
+for num in range(1, 4):
+    seed = 2
+    out_f = outdir/f'tennis_ctrl_grab_{num}.pkl'
 
-max_its = 1000
-ctrls = []
-lowest_losses = []
+    max_its = 1000
+    ctrls = []
+    lowest_losses = []
 
-with open(out_f, 'rb') as f:
-    load_data = pkl.load(f)
-    # ctrls = data['ctrls']
-    lowest_losses = load_data['lowest_losses']
+    with open(out_f, 'rb') as f:
+        load_data = pkl.load(f)
+        # ctrls = data['ctrls']
+        lowest_losses = load_data['lowest_losses']
 
-ctrls = lowest_losses.peekitem(0)[1][1]
-qs, vs = util.forward_sim(model, data, ctrls)
-system_states = np.hstack((qs, vs))
-save_str = str(savedir) + '/' + f'tennis_grab_{num}'
-np.save(save_str + '_ctrls.npy', ctrls)
-np.save(save_str + '_states.npy', system_states)
+    ctrls = lowest_losses.peekitem(0)[1][1]
+    qs, vs = util.forward_sim(model, data, ctrls)
+    system_states = np.hstack((qs, vs))
+    save_str = str(savedir) + '/' + f'tennis_grab_{num}'
+    np.save(save_str + '_states.npy', system_states)
+    if save_only_state:
+        np.save(save_str + '_ctrls.npy', ctrls)
