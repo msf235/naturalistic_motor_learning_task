062a5226bc6e30198ea25c07fbe2552cb07a6ff1
Matthew Farrell <matthew.farrell.235@gmail.com>
2024-12-09 13:16:45 +0900

    sync

--- config.py
+++ config.py
@@ -20,15 +20,24 @@ import argparse
 
 SIM_TIMESTEP = 1.0 / 60.0
 
+
 def set_np_formatting():
-    np.set_printoptions(edgeitems=30, infstr='inf',
-                        linewidth=4000, nanstr='nan', precision=2,
-                        suppress=False, threshold=10000, formatter=None)
+    np.set_printoptions(
+        edgeitems=30,
+        infstr="inf",
+        linewidth=4000,
+        nanstr="nan",
+        precision=2,
+        suppress=False,
+        threshold=10000,
+        formatter=None,
+    )
 
 
 def warn_task_name():
     raise Exception(
-        "Unrecognized task!\nTask should be one of: [BallBalance, Cartpole, CartpoleYUp, Ant, Humanoid, Anymal, FrankaCabinet, Quadcopter, ShadowHand, ShadowHandLSTM, ShadowHandFFOpenAI, ShadowHandFFOpenAITest, ShadowHandOpenAI, ShadowHandOpenAITest, Ingenuity]")
+        "Unrecognized task!\nTask should be one of: [BallBalance, Cartpole, CartpoleYUp, Ant, Humanoid, Anymal, FrankaCabinet, Quadcopter, ShadowHand, ShadowHandLSTM, ShadowHandFFOpenAI, ShadowHandFFOpenAITest, ShadowHandOpenAI, ShadowHandOpenAITest, Ingenuity]"
+    )
 
 
 def set_seed(seed, torch_deterministic=False):
@@ -41,13 +50,13 @@ def set_seed(seed, torch_deterministic=False):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
     if torch_deterministic:
         # refer to https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
-        os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
+        os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.deterministic = True
         torch.set_deterministic(True)
@@ -59,17 +68,21 @@ def set_seed(seed, torch_deterministic=False):
 
 
 def load_cfg(args):
-
-    cfg_path = f'embodied_pose/cfg/**/{args.cfg}.yaml'
+    cfg_path = f"embodied_pose/cfg/**/{args.cfg}.yaml"
     files = glob.glob(cfg_path, recursive=True)
-    assert(len(files) == 1)
+    assert len(files) == 1
     cfg_file = files[0]
 
-    with open(cfg_file, 'r') as f:
+    with open(cfg_file, "r") as f:
         cfg_all = yaml.load(f, Loader=yaml.SafeLoader)
 
-    cfg = {'name': cfg_all['name'], 'test_name': cfg_all.get('test_name', 'HumanoidSMPLRefv1'), 'env': cfg_all['env'], 'sim': cfg_all['sim']}
-    cfg_train = {'params': cfg_all['params']}
+    cfg = {
+        "name": cfg_all["name"],
+        "test_name": cfg_all.get("test_name", "HumanoidSMPLRefv1"),
+        "env": cfg_all["env"],
+        "sim": cfg_all["sim"],
+    }
+    cfg_train = {"params": cfg_all["params"]}
 
     # Override number of environments if passed on the command line
     if args.num_envs > 0:
@@ -91,36 +104,43 @@ def load_cfg(args):
     else:
         cfg["task"] = {"randomize": False}
 
-    cfg_dir = os.path.join('/tmp/embodied_pose' if args.tmp else args.results_dir, args.cfg)
-    cfg['cfg_dir'] = cfg_dir
+    cfg_dir = os.path.join(
+        "/tmp/embodied_pose" if args.tmp else args.results_dir, args.cfg
+    )
+    cfg["cfg_dir"] = cfg_dir
 
     # Set deterministic mode
     if args.torch_deterministic:
         cfg_train["params"]["torch_deterministic"] = True
 
-    exp_name = cfg_train["params"]["config"]['name']
+    exp_name = cfg_train["params"]["config"]["name"]
 
-    if args.experiment != 'Base':
+    if args.experiment != "Base":
         if args.metadata:
-            exp_name = "{}_{}_{}_{}".format(args.experiment, args.task_type, args.device, str(args.physics_engine).split("_")[-1])
+            exp_name = "{}_{}_{}_{}".format(
+                args.experiment,
+                args.task_type,
+                args.device,
+                str(args.physics_engine).split("_")[-1],
+            )
 
             if cfg["task"]["randomize"]:
                 exp_name += "_DR"
         else:
-             exp_name = args.experiment
+            exp_name = args.experiment
 
     # Override config name
-    cfg_train["params"]["config"]['name'] = exp_name
+    cfg_train["params"]["config"]["name"] = exp_name
 
     if args.resume:
         cfg_train["params"]["load_checkpoint"] = True
         cfg_train["params"]["config"]["load_checkpoint"] = True
 
     cfg_train["params"]["load_path"] = args.checkpoint
-        
+
     # Set maximum number of training iterations (epochs)
     if args.max_iterations > 0:
-        cfg_train["params"]["config"]['max_epochs'] = args.max_iterations
+        cfg_train["params"]["config"]["max_epochs"] = args.max_iterations
 
     cfg_train["params"]["config"]["num_actors"] = cfg["env"]["numEnvs"]
 
@@ -132,58 +152,63 @@ def load_cfg(args):
 
     cfg["args"] = cfg_train["params"]["config"]["args"] = args
 
-    cfg_train['params']['seed'] = set_seed(cfg_train['params'].get("seed", -1), cfg_train['params'].get("torch_deterministic", False))
+    cfg_train["params"]["seed"] = set_seed(
+        cfg_train["params"].get("seed", -1),
+        cfg_train["params"].get("torch_deterministic", False),
+    )
 
     if args.horovod:
-        cfg_train['params']['config']['multi_gpu'] = args.horovod
+        cfg_train["params"]["config"]["multi_gpu"] = args.horovod
 
     if args.horizon_length != -1:
-        cfg_train['params']['config']['horizon_length'] = args.horizon_length
+        cfg_train["params"]["config"]["horizon_length"] = args.horizon_length
 
     if args.minibatch_size != -1:
-        cfg_train['params']['config']['minibatch_size'] = args.minibatch_size
-    
+        cfg_train["params"]["config"]["minibatch_size"] = args.minibatch_size
+
     # Create default directories for weights and statistics
-    cfg_train['params']['config']['network_path'] = network_path = os.path.join(cfg_dir, 'models') 
-    cfg_train['params']['config']['log_path'] = log_path = os.path.join(cfg_dir, 'logs') 
-    cfg_train['params']['config']['wandb_dir'] = cfg_dir
-    cfg_train['params']['config']['train_dir'] = os.path.join(cfg_dir, 'train') 
-    cfg_train['params']['config']['device'] = args.rl_device
+    cfg_train["params"]["config"]["network_path"] = network_path = os.path.join(
+        cfg_dir, "models"
+    )
+    cfg_train["params"]["config"]["log_path"] = log_path = os.path.join(cfg_dir, "logs")
+    cfg_train["params"]["config"]["wandb_dir"] = cfg_dir
+    cfg_train["params"]["config"]["train_dir"] = os.path.join(cfg_dir, "train")
+    cfg_train["params"]["config"]["device"] = args.rl_device
 
     os.makedirs(network_path, exist_ok=True)
     os.makedirs(log_path, exist_ok=True)
 
     if args.test:
-        cfg['name'] = cfg['test_name']
+        cfg["name"] = cfg["test_name"]
     if args.motion_id is not None:
-        cfg['env']['motion_id'] = args.motion_id
+        cfg["env"]["motion_id"] = args.motion_id
     if args.export_dataset is not None:
-        cfg['env']['export_dataset'] = args.export_dataset
-    
-    cfg['env']['record'] = args.record
+        cfg["env"]["export_dataset"] = args.export_dataset
+
+    cfg["env"]["record"] = args.record
     if args.rec_fname is not None:
-        cfg['env']['rec_fname'] = args.rec_fname
+        cfg["env"]["rec_fname"] = args.rec_fname
     if args.num_rec_frames is not None:
-        cfg['env']['num_rec_frames'] = args.num_rec_frames
+        cfg["env"]["num_rec_frames"] = args.num_rec_frames
     if args.rec_fps is not None:
-        cfg['env']['rec_fps'] = float(args.rec_fps)
+        cfg["env"]["rec_fps"] = float(args.rec_fps)
     if args.camera is not None:
-        cfg['env']['camera'] = args.camera
+        cfg["env"]["camera"] = args.camera
 
     if args.cpu_motion_lib:
-        cfg['env']['gpu_motion_lib'] = not args.cpu_motion_lib
+        cfg["env"]["gpu_motion_lib"] = not args.cpu_motion_lib
 
     if args.test_motion_file is not None:
-        cfg['env']['test_motion_file'] = args.test_motion_file
+        cfg["env"]["test_motion_file"] = args.test_motion_file
 
     if args.motion_file_range is not None:
-        new_range = [int(x) for x in args.motion_file_range.split('-')]
+        new_range = [int(x) for x in args.motion_file_range.split("-")]
         if len(new_range) == 1:
             new_range.append(new_range[0] + 1)
-        cfg['env']['motion_file_range'] = new_range
+        cfg["env"]["motion_file_range"] = new_range
 
     if args.context_length is not None:
-        cfg['env']['context_length'] = args.context_length
+        cfg["env"]["context_length"] = args.context_length
 
     return cfg, cfg_train
 
@@ -191,39 +216,53 @@ def load_cfg(args):
 def get_arg_parser():
     # parser = argparse.ArgumentParser()
     # parser.add_argument("--rerun", action="store_true", default=False,
-            # help="Retrain model instead of loading previous file")
+    # help="Retrain model instead of loading previous file")
     # parser.add_argument("--savefile", type=str, default=None,
-            # help="Path to the saved data.")
+    # help="Path to the saved data.")
     parser = argparse.ArgumentParser()
-    parser.add_argument("--configfile", type=str, default=None,
-            help="Path to the configuration file (YAML formatted).")
-    parser.add_argument("--verbose", action="store_true",
-                        help="increase output verbosity")
-    parser.add_argument("--rerun", action="store_true",
-                        help="Retrain model instead of loading.")
-    parser.add_argument("--name", type=str, default=None,
-            help="Name of experiment, used to name output files etc.")
-    parser.add_argument("--seed", type=int, default=2,
-            help="Random seed.")
-    parser.add_argument("--task-phase", type=int, default=1,
-            help="Task phase (from 1 to 7).")
-    parser.add_argument("--render", action="store_true",
-            help="Render the agent.")
-    parser.add_argument("--plot_every", type=int,
-                        help="Update diagnostic plot every X iterations.")
-    parser.add_argument("--render_every", type=int,
-                        help="Render environment every X iterations.")
+    parser.add_argument(
+        "--configfile",
+        type=str,
+        default=None,
+        help="Path to the configuration file (YAML formatted).",
+    )
+    parser.add_argument(
+        "--verbose", action="store_true", help="increase output verbosity"
+    )
+    parser.add_argument(
+        "--rerun", action="store_true", help="Retrain model instead of loading."
+    )
+    parser.add_argument(
+        "--name",
+        type=str,
+        default=None,
+        help="Name of experiment, used to name output files etc.",
+    )
+    parser.add_argument("--seed", type=int, default=2, help="Random seed.")
+    parser.add_argument(
+        "--task-phase", type=int, default=1, help="Task phase (from 1 to 7)."
+    )
+    parser.add_argument("--render", action="store_true", help="Render the agent.")
+    parser.add_argument(
+        "--plot_every", type=int, help="Update diagnostic plot every X iterations."
+    )
+    parser.add_argument(
+        "--render_every", type=int, help="Render environment every X iterations."
+    )
     return parser
 
-def get_config(config_file):
+
+def get_config(config_file) -> dict:
     """Get configuration from yaml configuration file."""
 
-    with open(config_file, 'r') as stream:
+    with open(config_file, "r") as stream:
         try:
             cfg = yaml.safe_load(stream)
+            return cfg
         except yaml.YAMLError as exc:
             print(exc)
-    return cfg
+    return {}
+
 
 def inp_to_num(in_val):
     """Convert input value to number. Typically, the input value in_val will be
@@ -240,6 +279,3 @@ def inp_to_num(in_val):
         return float(in_val)
     except ValueError:
         return in_val
-
-
-
--- run_experiment.py
+++ run_experiment.py
@@ -14,7 +14,7 @@ import config
 
 args = config.get_arg_parser().parse_args()
 vargs = vars(args)
-params = config.get_config(args.configfile)['params']
+params = config.get_config(args.configfile)["params"]
 # Since numbers in scientific notation are converted to a string from yaml,
 # need to convert these to a number.
 params = {k: config.inp_to_num(v) for k, v in params.items()}
@@ -28,48 +28,57 @@ DEFAULT_CAMERA_CONFIG = {
 }
 
 name = args.name
-out_f = (Path('output') / name).with_suffix('.pkl')
+out_f = (Path("output") / name).with_suffix(".pkl")
 
 out_f.parent.mkdir(parents=True, exist_ok=True)
 
-Tf = params['Tf']
+Tf = params["Tf"]
 # Tf = 10*Tf
 
 CTRL_STD = 0
 CTRL_RATE = 1
 
 if args.render:
-    render_mode = 'human'
+    render_mode = "human"
 else:
     # render_mode = 'rgb_array'
-    render_mode = 'None'
+    render_mode = "None"
 
-keyframe = 'tpose1'
+keyframe = "tpose1"
 
 env = humanoid2d.Humanoid2dEnv(
     render_mode=render_mode,
     frame_skip=1,
     default_camera_config=DEFAULT_CAMERA_CONFIG,
-    reset_noise_scale=params['reset_noise_scale'],
-    xml_file=params['xml_file'],
-    keyframe_name=keyframe,)
+    reset_noise_scale=params["reset_noise_scale"],
+    xml_file=params["xml_file"],
+    keyframe_name=keyframe,
+)
 model = env.model
 data = env.data
 
 dt = model.opt.timestep
 # burn_step = int(.09 / dt)
 # burn_step = int(.001 / dt)
-burn_step = int(.01 / dt)
+burn_step = int(0.01 / dt)
 print(burn_step)
-reset = lambda: opt_utils.reset_with_lqr(env, args.seed, burn_step,
-                                          8*burn_step,
-                                          # 5000,
-                                          params['balance_cost'],
-                                          params['joint_cost'],
-                                          params['root_cost'],
-                                          params['foot_cost'],
-                                          params['ctrl_cost'],
-                                         )
+
+
+def reset():
+    return opt_utils.reset_with_lqr(
+        env,
+        args.seed,
+        burn_step,
+        8 * burn_step,
+        # 5000,
+        params["balance_cost"],
+        params["joint_cost"],
+        params["root_cost"],
+        params["foot_cost"],
+        params["ctrl_cost"],
+    )
+
+
 ctrls_burn_in = reset()
 # while True:
 # env.reset(seed=args.seed, options={'n_steps': 0, 'render': False})
@@ -83,20 +92,21 @@ joints = opt_utils.get_joint_ids(model)
 acts = opt_utils.get_act_ids(model)
 
 # body_dof = joints['body']['dofadrs_without_root']
-body_dof = joints['body']['dofadrs']
+body_dof = joints["body"]["dofadrs"]
 # body_dof_unflat = [joints['dofadr'][k] for k in bodyj_id] # Body dof ids
 # body_dof = [dof for dofs in body_dof_unflat for dof in dofs] # flattened
 # body_dof = opt_utils.convert_dofadr(model, None, bodyj_id, True)
 
-out_idx = arm_t.get_idx_sets(env, params['name'])
-sites = out_idx['sites']
-out_time = arm_t.get_times(env, params['name'], Tf)
+out_idx = arm_t.get_idx_sets(env, params["name"])
+sites = out_idx["sites"]
+out_time = arm_t.get_times(env, params["name"], Tf)
 
 
-out_traj = arm_t.make_traj_sets(env, params['name'], Tk, seed=args.seed)
-out_traj['q_targ_masks'] = [params['joint_penalty_factor'] * x
-                            for x in out_traj['q_targ_masks']]
-targ_trajs = out_traj['targ_trajs']
+out_traj = arm_t.make_traj_sets(env, params["name"], Tk, seed=args.seed)
+out_traj["q_targ_masks"] = [
+    params["joint_penalty_factor"] * x for x in out_traj["q_targ_masks"]
+]
+targ_trajs = out_traj["targ_trajs"]
 # plt.plot(tt, targ_trajs[0][:,1])
 # plt.plot(tt, targ_trajs[1][:,1]); plt.show()
 
@@ -105,29 +115,29 @@ noisev = arm_t.make_noisev(model, args.seed, Tk, CTRL_STD, CTRL_RATE)
 # breakpoint()
 
 # ctrls, K = opt_utils.get_stabilized_ctrls(
-    # model, data, Tk, noisev, data.qpos.copy(),
-    # # acts['all'],
-    # out_idx['stabilize_act_idx'],
-    # # body_dof,
-    # out_idx['stabilize_jnt_idx'],
-    # # free_ctrls=np.zeros((Tk, len(acts['adh']))),
-    # free_ctrls=np.zeros((Tk, len(out_idx['free_act_idx']))),
-    # balance_cost=params['balance_cost'],
-    # joint_cost=params['joint_cost'],
-    # root_cost=params['root_cost'],
-    # foot_cost=params['foot_cost'],
-    # ctrl_cost=params['ctrl_cost']
+# model, data, Tk, noisev, data.qpos.copy(),
+# # acts['all'],
+# out_idx['stabilize_act_idx'],
+# # body_dof,
+# out_idx['stabilize_jnt_idx'],
+# # free_ctrls=np.zeros((Tk, len(acts['adh']))),
+# free_ctrls=np.zeros((Tk, len(out_idx['free_act_idx']))),
+# balance_cost=params['balance_cost'],
+# joint_cost=params['joint_cost'],
+# root_cost=params['root_cost'],
+# foot_cost=params['foot_cost'],
+# ctrl_cost=params['ctrl_cost']
 # )[:2]
 # while True:
-    # reset()
-    # util.forward_sim_render(env, ctrls)
+# reset()
+# util.forward_sim_render(env, ctrls)
 
-t_incr = params['t_incr']
-amnt_to_incr = int(t_incr/dt)
-grad_update_every = params['grad_update_every']
-grad_trunc_tk = int(params['grad_window_t']/(grad_update_every*dt))
+t_incr = params["t_incr"]
+amnt_to_incr = int(t_incr / dt)
+grad_update_every = params["grad_update_every"]
+grad_trunc_tk = int(params["grad_window_t"] / (grad_update_every * dt))
 
-Tke = int(params['t_after'] / dt)
+Tke = int(params["t_after"] / dt)
 
 # TODO: move this out
 
@@ -135,110 +145,139 @@ if args.rerun or not out_f.exists():
     ### Get initial stabilizing controls
     reset()
     # stab_ctrls_idx = {k: out_idx[k] for k in
-                      # ['let_go_ids', 'contact_check_list',
-                       # 'adh_ids']}
+    # ['let_go_ids', 'contact_check_list',
+    # 'adh_ids']}
     # stab_ctrls_idx.update({'let_go_times': out_time['let_go_times']})
     ctrls, K = opt_utils.get_stabilized_ctrls(
-        model, data, Tk, noisev, data.qpos.copy(), acts['not_adh'],
+        model,
+        data,
+        Tk,
+        noisev,
+        data.qpos.copy(),
+        acts["not_adh"],
         body_dof,
-        free_ctrls=np.zeros((Tk, len(acts['adh']))),
-        balance_cost=params['balance_cost'],
-        joint_cost=params['joint_cost'],
-        root_cost=params['root_cost'],
-        foot_cost=params['foot_cost'],
-        ctrl_cost=params['ctrl_cost']
+        free_ctrls=np.zeros((Tk, len(acts["adh"]))),
+        balance_cost=params["balance_cost"],
+        joint_cost=params["joint_cost"],
+        root_cost=params["root_cost"],
+        foot_cost=params["foot_cost"],
+        ctrl_cost=params["ctrl_cost"],
     )[:2]
     # ctrls[:, tennis_idxs['adh_left_hand']] = left_adh_act_vals
     # while True:
-        # reset()
-        # util.forward_sim_render(env, ctrls)
+    # reset()
+    # util.forward_sim_render(env, ctrls)
     # arm_t.forward_to_contact(env, ctrls, render=True)
     # reset()
-    del out_idx['free_act_idx']
+    del out_idx["free_act_idx"]
 
-    arm_targ_params = {k: params[k] for k in
-                       ['max_its', 'optimizer', 'lr', 'lr2', 'it_lr2',
-                        'grad_update_every', 'incr_every',
-                        # 't_incr',
-                        'grab_phase_it',
-                        'balance_cost', 'joint_cost',
-                        # 'grad_window_t'
-                       ]}
+    arm_targ_params = {
+        k: params[k]
+        for k in [
+            "max_its",
+            "optimizer",
+            "lr",
+            "lr2",
+            "it_lr2",
+            "grad_update_every",
+            "incr_every",
+            # 't_incr',
+            "grab_phase_it",
+            "balance_cost",
+            "joint_cost",
+            # 'grad_window_t'
+        ]
+    }
     ctrls, lowest_losses = arm_t.arm_target_traj(
         env,
         **out_traj,
         **arm_targ_params,
         **out_idx,
         **out_time,
-        ctrls=ctrls, grad_trunc_tk=grad_trunc_tk,
-        seed=args.seed, ctrl_rate=CTRL_RATE, ctrl_std=CTRL_STD,
+        ctrls=ctrls,
+        grad_trunc_tk=grad_trunc_tk,
+        seed=args.seed,
+        ctrl_rate=CTRL_RATE,
+        ctrl_std=CTRL_STD,
         Tk=Tk,
         keep_top=10,
         amnt_to_incr=amnt_to_incr,
-        phase_2_it=params['max_its']+1,
+        phase_2_it=params["max_its"] + 1,
         plot_every=args.plot_every,
         render_every=args.render_every,
     )
     ctrls = np.vstack((ctrls, ctrls_burn_in))
     ctrls_end = np.zeros((Tke, model.nu))
-    with open(out_f, 'wb') as f:
-        pkl.dump({'ctrls': ctrls, 'lowest_losses': lowest_losses,
-                  'ctrls_burn_in': ctrls_burn_in, 'ctrls_end': ctrls_end}, f)
+    with open(out_f, "wb") as f:
+        pkl.dump(
+            {
+                "ctrls": ctrls,
+                "lowest_losses": lowest_losses,
+                "ctrls_burn_in": ctrls_burn_in,
+                "ctrls_end": ctrls_end,
+            },
+            f,
+        )
 else:
-    with open(out_f, 'rb') as f:
+    with open(out_f, "rb") as f:
         load_data = pkl.load(f)
-    ctrls = load_data['ctrls']
-    ctrls_burn_in = load_data['ctrls_burn_in']
-    lowest_losses = load_data['lowest_losses']
+    ctrls = load_data["ctrls"]
+    ctrls_burn_in = load_data["ctrls_burn_in"]
+    lowest_losses = load_data["lowest_losses"]
 
 
 ctrls = lowest_losses.peekitem(0)[1][1]
 ctrls_end = np.zeros((Tke, model.nu))
 reset()
 hxs, qs = arm_t.forward_with_sites(env, ctrls, sites, render=False)
-fig, axs = plt.subplots(2, len(sites), figsize=(8,4))
+fig, axs = plt.subplots(2, len(sites), figsize=(8, 4))
 if len(sites) == 1:
     axs = axs.reshape((2, 1))
 q_targs_masked = []
 qs_list = []
 for k in range(len(sites)):
-    q_targs_masked_tmp = out_traj['q_targs'][k].copy()
-    q_targs_masked_tmp[out_traj['q_targ_masks'][k] == 0] = np.nan
+    q_targs_masked_tmp = out_traj["q_targs"][k].copy()
+    q_targs_masked_tmp[out_traj["q_targ_masks"][k] == 0] = np.nan
     q_targs_masked.append(q_targs_masked_tmp)
     qs_tmp = qs.copy()
-    qs_tmp[out_traj['q_targ_masks'][k] == 0] = np.nan
+    qs_tmp[out_traj["q_targ_masks"][k] == 0] = np.nan
     qs_list.append(qs_tmp)
-arm_t.show_plot(axs, hxs, tt, out_traj['targ_trajs'],
-                out_traj['targ_traj_masks'],
-                sites, out_idx['site_grad_idxs'], qvals=qs_list,
-                qtargs=q_targs_masked)
+arm_t.show_plot(
+    axs,
+    hxs,
+    tt,
+    out_traj["targ_trajs"],
+    out_traj["targ_traj_masks"],
+    sites,
+    out_idx["site_grad_idxs"],
+    qvals=qs_list,
+    qtargs=q_targs_masked,
+)
 plt.show()
 show_sim_cnt = 0
 # while True:
-    # print("shown simulation {} times".format(show_sim_cnt))
-    # reset()
-    # arm_t.forward_with_sites(env, ctrls_full, sites, render=True)
-    # time.sleep(2)
-    # show_sim_cnt += 1
+# print("shown simulation {} times".format(show_sim_cnt))
+# reset()
+# arm_t.forward_with_sites(env, ctrls_full, sites, render=True)
+# time.sleep(2)
+# show_sim_cnt += 1
 
 
 phase = args.task_phase
-datadir = (Path('data') / name)
+datadir = Path("data") / name
 datadir.mkdir(parents=True, exist_ok=True)
 
-shutil.copy('humanoid.xml', datadir)
-shutil.copy('humanoid_and_basic.xml', datadir)
-shutil.copy('basic_scene.xml', datadir)
+shutil.copy("humanoid.xml", datadir)
+shutil.copy("humanoid_and_basic.xml", datadir)
+shutil.copy("basic_scene.xml", datadir)
 
 ctrls_full = np.vstack((ctrls, ctrls_end))
 reset()
 qs, vs, ss = util.forward_sim(model, data, ctrls_full)
 states = np.hstack((qs, vs))
 
-np.save(datadir / 'states_{}.npy'.format(args.seed), states)
+np.save(datadir / "states_{}.npy".format(args.seed), states)
 
 if phase in (1, 5):
-    np.save(datadir / 'ctrls_{}.npy'.format(args.seed), ctrls_full)
-    np.save(datadir / 'sensors_{}.npy'.format(args.seed), ss)
-
-
+    np.save(datadir / "ctrls_{}.npy".format(args.seed), ctrls_full)
+    np.save(datadir / "sensors_{}.npy".format(args.seed), ss)
