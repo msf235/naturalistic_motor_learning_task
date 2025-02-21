7d90ba5853dda13b61f1f32a94233464fc4bad3f
Matthew Farrell <matthew.farrell.235@gmail.com>
2025-02-21 23:26:27 +0900

    sync

--- arm_targ_traj.py
+++ arm_targ_traj.py
@@ -478,10 +478,10 @@ def tennis_traj(model, data, Tk):
         Tk_left_5,
         density_fn="",
     )
-    p0 = np.array([0.5, 0.0, 1.5])
-    p1 = np.array([1, -0.5, 2])
-    p2 = np.array([0, 0.5, 2.5])
-    p3 = np.array([0, -0.9, 2.25])
+    p0 = np.array([0.25, -0.1, 1.0])
+    p1 = np.array([0.5, -0.5, 2])
+    p2 = np.array([-0.02, 0.25, 2.0])
+    p3 = np.array([-0.02, -0.9, 2.25])
     t_vals = np.linspace(0, 1, Tk_left_5)
     arc_traj_below = np.array([bezier(ease_in_out(t), p0, p1, p2, p3) for t in t_vals])
 
@@ -499,18 +499,26 @@ def tennis_traj(model, data, Tk):
     left_arm_traj_below = np.concatenate(
         (grab_traj_below, setup_traj_below, arc_traj_below), axis=0
     )
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111, projection="3d")
-    # ax.plot(traj_below[:, 0], traj_below[:, 1], traj_below[:, 2], "x-")
-    # ax.plot(left_arm_traj[:, 0], left_arm_traj[:, 1], left_arm_traj[:, 2], "x-")
-    # ax.set_xlabel("X")
-    # ax.set_xlim([-1, 1])
-    # ax.set_ylabel("Y")
-    # ax.set_ylim([-2.5, 1])
-    # ax.set_zlabel("Z")
-    # ax.set_zlim([0, 3])
-    # plt.show()
-    # breakpoint()
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    ax.plot(
+        left_arm_traj_below[:, 0],
+        left_arm_traj_below[:, 1],
+        left_arm_traj_below[:, 2],
+    )
+    ax.plot(
+        left_arm_traj[:, 0],
+        left_arm_traj[:, 1],
+        left_arm_traj[:, 2],
+    )
+    ax.set_xlabel("X")
+    ax.set_xlim([-1, 1])
+    ax.set_ylabel("Y")
+    ax.set_ylim([-2.5, 1])
+    ax.set_zlabel("Z")
+    ax.set_zlim([0, 3])
+    plt.show()
+    breakpoint()
 
     time_dict = dict(
         Tk_right_1=Tk_right_1,
