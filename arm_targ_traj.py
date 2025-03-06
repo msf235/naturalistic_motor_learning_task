4f3b66e79bd9bb112e6ab688d5f0838d3e21c1f4
Matthew Farrell <matthew.farrell.235@gmail.com>
2025-03-06 22:14:40 +0900

    sync

--- arm_targ_traj.py
+++ arm_targ_traj.py
@@ -528,6 +528,7 @@ def tennis_traj(model, data, Tk):
     p3 = np.array([0, -0.9, 2.25])
     t_vals = np.linspace(0, 1, Tk_left_4 + Tk_left_5)
     arc_traj_below = np.array([bezier(ease_in_out(t), p0, p1, p2, p3) for t in t_vals])
+    arc_traj_below = directional_distance_normalize(arc_traj_vs, arc_traj_below)
 
     setup_traj = np.zeros((Tk_left_3, 3))
     s = np.linspace(0, 1, Tk_left_3)
@@ -536,6 +537,7 @@ def tennis_traj(model, data, Tk):
     setup_traj_below = grab_traj_below[-1] + s * (
         arc_traj_below[0] - grab_traj_below[-1]
     )
+    setup_traj_below = directional_distance_normalize(setup_traj, setup_traj_below)
 
     left_arm_traj = np.concatenate(
         (grab_traj, setup_traj, arc_traj_vs, arc_traj_vs2), axis=0
