cd539cb1405bdca974441360166c8e61baabba4b
Matthew Farrell <matthew.farrell.235@gmail.com>
2025-03-23 22:28:48 +0900

    added run file

---
+++ run.sh
@@ -0,0 +1,5 @@
+commit_id=$(git rev-parse --short HEAD)
+echo $commit_id
+py run_experiment.py --configfile="exp_configs/tennis_serve_med.yaml" \
+  --plot_every=1 --render_every=1 --seed=8 --task-phase=1 --rerun \
+  --name="$commit_id"
