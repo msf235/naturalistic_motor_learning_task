2fc72b296416c6169ac4323a85d25bdbef405f7f
Matthew Farrell <matthew.farrell.235@gmail.com>
2025-03-31 23:39:34 +0900

    hopefully fixed max_its

--- arm_targ_traj.py
+++ arm_targ_traj.py
@@ -909,8 +909,6 @@ def make_traj_sets(
         for k, mask in enumerate(targ_traj_mask_lists)
     }
 
-    breakpoint()
-
     def get_q_pos_and_vel_data(joint_targs_file):
         q_pos_data = get_data_from_qtarg_file(joint_targs_file, dt)
         q_pos_targs = q_pos_data["targ_val"]
