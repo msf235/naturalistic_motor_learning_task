2427d95d1dba93efd55a2ccb82a27873cee8e6ff
Matthew Farrell <matthew.farrell.235@gmail.com>
2025-04-01 18:08:33 +0900

    sync

--- arm_targ_traj.py
+++ arm_targ_traj.py
@@ -500,27 +500,27 @@ def tennis_traj(model, data, Tk, Tk_left_3=None):
         right_arm_traj, right_arm_traj_orient
     )
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection="3d")
-    ax.plot(
-        right_arm_traj_orient[:, 0],
-        right_arm_traj_orient[:, 1],
-        right_arm_traj_orient[:, 2],
-    )
-    ax.plot(right_arm_traj[:, 0], right_arm_traj[:, 1], right_arm_traj[:, 2])
-    # Do scatter plots of points p0 through p3
-    ax.scatter(p0[0], p0[1], p0[2], c="red")
-    ax.scatter(p1[0], p1[1], p1[2], c="red")
-    ax.scatter(p2[0], p2[1], p2[2], c="red")
-    ax.scatter(p3[0], p3[1], p3[2], c="red")
-    ax.set_xlabel("X")
-    ax.set_xlim([-1, 1])
-    ax.set_ylabel("Y")
-    ax.set_ylim([-2.5, 1])
-    ax.set_zlabel("Z")
-    ax.set_zlim([0, 3])
-    plt.show()
-    breakpoint()
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection="3d")
+    # ax.plot(
+    #     right_arm_traj_orient[:, 0],
+    #     right_arm_traj_orient[:, 1],
+    #     right_arm_traj_orient[:, 2],
+    # )
+    # ax.plot(right_arm_traj[:, 0], right_arm_traj[:, 1], right_arm_traj[:, 2])
+    # # Do scatter plots of points p0 through p3
+    # ax.scatter(p0[0], p0[1], p0[2], c="red")
+    # ax.scatter(p1[0], p1[1], p1[2], c="red")
+    # ax.scatter(p2[0], p2[1], p2[2], c="red")
+    # ax.scatter(p3[0], p3[1], p3[2], c="red")
+    # ax.set_xlabel("X")
+    # ax.set_xlim([-1, 1])
+    # ax.set_ylabel("Y")
+    # ax.set_ylim([-2.5, 1])
+    # ax.set_zlabel("Z")
+    # ax.set_zlim([0, 3])
+    # plt.show()
+    # breakpoint()
 
     ##---- Left arm (ball)
 
